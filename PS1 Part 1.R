8c5dba91eed704522179cf9f20ee6180e1f5375a
Alexander-Rados <radosalexander@gmail.com>
2020-02-01 18:11:15 -0600

    Update PS1 Part1
    Created new graphs related to airlines and the percent of flights cancelled by airlines, along with the number of flights flown.

--- PS1 Part 1.R
+++ PS1 Part 1.R
@@ -1,8 +1,13 @@
 library(tidyverse)
 library(tidyr)
+library(scales)
 
 # creating a dataset specifically for Austin departures
 aus_depart = filter(ABIA, Origin == "AUS")
+
+# adding column of how early/late the flight was
+ABIA = mutate(ABIA,
+              punctuality = ArrDelay)
 
 # grouping by destination and getting number of cancelled flights
 # figuring out portion of flights cancelled by destination
@@ -13,6 +18,41 @@ by_dest = aus_depart %>%
     cancel = sum(Cancelled)
   ) %>%
   filter(count>1000)
+
+# grouping by airline and getting number of cancelled flights
+by_airline = ABIA %>%
+  group_by(UniqueCarrier) %>%
+  summarise(
+    count = n(),
+    cancel = sum(Cancelled),
+    percent_cancel = (cancel)/(count + cancel),
+    punctuality = mean(ArrDelay)
+  ) %>%
+  filter(count>1000)
+
+# plotting cancellations by airline with a fill gradient of red and blue
+ggplot(data = by_airline) +
+  geom_bar(mapping = aes(x = reorder(UniqueCarrier, percent_cancel), 
+                         y = percent_cancel, fill = count), stat = "identity") +
+  xlab("Airline") + ylab("Number of Flights") + 
+  scale_fill_gradient(low = "blue", high = "red", space = "Lab") +
+  labs(fill = "Percent of Flights Cancelled") +
+  coord_flip()
+
+# plotting cancellations by airline without a fill gradient
+ggplot(data = by_airline) +
+  geom_bar(mapping = aes(x = reorder(UniqueCarrier, percent_cancel), 
+                         y = percent_cancel, fill = count), stat = "identity") +
+  xlab("Airline") + ylab("Percent of Flights Cancelled") + 
+  labs(fill = "Number of Flights") +
+  coord_flip()
+
+ggplot(data = by_airline) +
+  geom_bar(mapping = aes(x = reorder(UniqueCarrier, count), 
+                         y = count, fill = percent_cancel), stat = "identity") +
+  xlab("Airline") + ylab("Number of Flights") + 
+  labs(fill = "Percent of Flights Cancelled") +
+  coord_flip()
 
 # creating a new column based on proportion of flights that weren't cancelled
 # by destination
@@ -26,6 +66,14 @@ ggplot(data = by_dest) +
   xlab("Airport Destination") + ylab("Number of Flights") + 
   labs(fill = "Percent of Flights Not Cancelled") +
   coord_flip()
+
+ggplot(data = by_dest) +
+  geom_bar(mapping = aes(x = reorder(Dest, percent_flown), y = percent_flown, 
+                         fill = count), stat = "identity") +
+  xlab("Airport Destination") + ylab("Number of Flights") + 
+  labs(fill = "Percent of Flights Not Cancelled") +
+  coord_flip()
+
 
 # creating a map of the US
 usa = map_data(('state'), region = '.')
@@ -61,16 +109,3 @@ ggplot(data = car_del) +
 
 ggplot(data = ABIA_no_na) +
   geom_point(mapping = aes(x = CRSDepTime, y = CarrierDelay), position = "jitter")
-
-ggplot(data = ABIA_no_na) +
-  geom_point(mapping = aes(x = CRSDepTime, y = WeatherDelay))
-
-ggplot(data = ABIA_no_na) +
-  geom_point(mapping = aes(x = CRSDepTime, y = LateAircraftDelay))
-
-ggplot(data = ABIA_no_na) +
-  geom_point(mapping = aes(x = CRSDepTime, y = LateAircraftDelay, color = AirTime))
-
-ggplot(data = ABIA_no_na) +
-  geom_point(mapping = aes(x = CRSDepTime, y = WeatherDelay)) +
-  facet_wrap(~ Month)
